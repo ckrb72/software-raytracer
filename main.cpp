535b37d2eca5c2df5dea80e214675d914fa4c457
ckrb72 <krck70153@gmail.com>
2025-03-26 21:56:46 -0400

    added dielectric material

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main() {
 
     std::shared_ptr<raytracer::material> material_ground = std::make_shared<raytracer::lambertian>(glm::vec3(0.8, 0.8, 0.0));
     std::shared_ptr<raytracer::material> material_center = std::make_shared<raytracer::lambertian>(glm::vec3(0.1, 0.2, 0.5));
-    std::shared_ptr<raytracer::material> material_left = std::make_shared<raytracer::metal>(glm::vec3(0.8, 0.8, 0.8));
-    std::shared_ptr<raytracer::material> material_right = std::make_shared<raytracer::metal>(glm::vec3(0.8, 0.6, 0.2)); 
+    std::shared_ptr<raytracer::material> material_left = std::make_shared<raytracer::dielectric>(1.0f / 1.33f);
+    std::shared_ptr<raytracer::material> material_right = std::make_shared<raytracer::metal>(glm::vec3(0.8, 0.6, 0.2), 1.0); 
 
     world.add(std::make_shared<raytracer::sphere>(glm::vec3(0.0, 0.0, -1.2), 0.5, material_center));
     world.add(std::make_shared<raytracer::sphere>(glm::vec3(0.0, -100.5, -1.0), 100, material_ground));
@@ -22,7 +22,7 @@ int main() {
 
     raytracer::camera camera;
     camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 1280;
+    camera.image_width = 400;
     camera.samples_per_pixel = 500;
     camera.max_depth = 50;
 
--- material.h
+++ material.h
@@ -35,17 +35,55 @@ namespace raytracer
     {
         private:
             glm::vec3 albedo;
+            float fuzz;
 
         public:
-        metal(const glm::vec3& albedo): albedo(albedo) {}
+        metal(const glm::vec3& albedo, float fuzz): albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
         bool scatter(const ray& r_in, const hit_record& rec, glm::vec3& attenuation, ray& scattered) const override
         {
             glm::vec3 reflected = glm::reflect(r_in.direction(), rec.normal);
+            reflected = glm::normalize(reflected) + (fuzz * random_unit_vector());
             scattered = ray(rec.point, reflected);
             attenuation = albedo;
-            return true;
+            return glm::dot(scattered.direction(), rec.normal) > 0;
         }
-
     };
+
+    class dielectric : public material
+    {
+        private:
+            float refraction_index;
+
+        public:
+
+            dielectric(float refraction_index): refraction_index(refraction_index) {}
+
+            bool scatter(const ray& r_in, const hit_record& rec, glm::vec3& attenuation, ray& scattered) const override
+            {
+                attenuation = glm::vec3(1.0, 1.0, 1.0);
+                float ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+                glm::vec3 unit_direction = glm::normalize(r_in.direction());
+                glm::vec3 refracted = glm::refract(unit_direction, rec.normal, ri);
+
+                float cos_theta = std::fmin(glm::dot(-unit_direction, rec.normal), 1.0);
+                float sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+                glm::vec3 direction;
+
+                if(ri * sin_theta > 1.0)
+                {
+                    direction = glm::reflect(unit_direction, rec.normal);
+                }
+                else
+                {
+                    direction = glm::refract(unit_direction, rec.normal, ri);
+                }
+
+                scattered = ray(rec.point, direction);
+
+                return true;
+            }
+    };
+
 }
